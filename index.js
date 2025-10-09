47ddae7c745afe6dd27464ee015d9ec7abb5cd06
qwikster <ocoincs@gmail.com>
2025-10-09 17:25:17 -0400

    onlick

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function getNum(min, max) {
 function surge() {
     var number = document.getElementById("number");
     var pick = getNum(0, 9999);
-    String(pick).padStart(4, "0");
+    pick = String(pick).padStart(4, "0");
     number.textContent = pick;
 }
