3b57762a642f03e69beccd693d0151a4e5fca7d8
qwikster <ocoincs@gmail.com>
2025-10-09 17:20:02 -0400

    semicolons exist waow

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function getNum(min, max) {
     min = Math.ceil(min);
-    max = Math.floor(max)
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function surge() {
-    var number = document.getElementById("number")
-    var pick = getNum(0, 9999)
-    String(pick).padStart(4, "0")
-    number.textContent = pick
+    var number = document.getElementById("number");
+    var pick = getNum(0, 9999);
+    String(pick).padStart(4, "0");
+    number.textContent = pick;
 }
