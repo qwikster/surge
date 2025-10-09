00e39b45d8f1e1dbe1ed4aab60bf1ff6ddc4c343
qwikster <ocoincs@gmail.com>
2025-10-09 17:09:48 -0400

    element set test

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function pass() {
-    pass
+function getNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-pass();
+function surge() {
+    var number = document.getElementById("number")
+    var pick = Math.random(0, 9999)
+    String(pick).padStart(4, "0")
+    number.textContent = pick
+}
